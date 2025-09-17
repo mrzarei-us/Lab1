577fd570b14f651e4e514214ced9c44a84d7e9da
mrzarei-us <mzarei@my.smccd.edu>
2025-09-17 18:10:24 +0330

    Initial Commit

---
+++ Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by Mohammadreza Zarei on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1Tests/Lab1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1Tests.swift
+//  Lab1Tests
+//
+//  Created by Mohammadreza Zarei on 9/17/25.
+//
+
+import Testing
+@testable import Lab1
+
+struct Lab1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
